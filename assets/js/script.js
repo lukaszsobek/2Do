b3b760020d19c6ecf1fbb5a866cf03e3a7c116d1
Łukasz Sobek <lukaszsobek@me.com>
2017-09-10 22:23:34 +0200

    - inserts new item from input value on hitting enter

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,27 @@ $("li").on("click", function() {
 	$(this).toggleClass("markedDone")
 })
 
+
+
 // remove item on click
-$("span").on("click", function(event) {
+$("span").on("click", function(e) {
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove()
 	})
-	event.stopPropagation()
+	e.stopPropagation()
 
+})
+
+
+
+// add new item to the ul list 
+$("input[type='text']").on("keypress", function(e) {
+	if(e.which == 13) {
+		let textValue = $(this).val()
+		$(this).val("")
+		$("ul").append("<li>" + textValue + "<span>x</span></li>")
+
+	}
 })
 
 
