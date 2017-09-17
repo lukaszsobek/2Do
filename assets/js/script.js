f709f56e2b5f80646287b9029d1c0d850545ca3d
Łukasz Sobek <lukaszsobek@me.com>
2017-09-17 13:27:34 +0200

    - adds saving to local storage - fixes bug preventing adding items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+"use strict"
+
+let LocalKey = "lukaszsobek_2Do_application"
+let toDoList = []
+
+
+// save item to local storage
+function saveItem(item) {
+
+	// we do need to get items already stored though
+	let itemList = localStorage.getItem(LocalKey)
+	itemList = itemList ? JSON.parse(itemList) : []
+	itemList.push(item)
+	itemList = JSON.stringify(itemList)
+	localStorage.setItem(LocalKey,itemList)
+	console.log(localStorage.getItem(LocalKey))
+
+}
+
+
 // mark as done with css class
 $("ul").on("click", "li", function() {
 	$(this).toggleClass("markedDone")
@@ -15,9 +35,10 @@ $("ul").on("click", "span", function(e) {
 
 
 // add new item to the ul list 
-$("input[type='text']").on("keypress", function(e) {
+$("input[type='search']").on("keypress", function(e) {
 	if(e.which == 13 && $(this).val() != "") {
 		let textValue = $(this).val()
+		saveItem(textValue)
 		$(this).val("")
 		$("ul").append("<li>" + textValue + "<span><i class='fa fa-trash-o' aria-hidden='true'></i></span></li>")
 
